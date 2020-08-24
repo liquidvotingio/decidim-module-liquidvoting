fb1142cea2a63c64d3ae948d0d12998fe9b98063
Pau Pérez Fabregat <saulopefa@gmail.com>
2020-08-24 11:01:07 +0200

    Update app/controllers/decidim/action_delegator/admin/application_controller.rb
    Refactor #permission_class_chain to follow the convention
    
    Co-authored-by: Vera Rojman <8806781+verarojman@users.noreply.github.com>

--- app/controllers/decidim/action_delegator/admin/application_controller.rb
+++ app/controllers/decidim/action_delegator/admin/application_controller.rb
@@ -6,8 +6,12 @@ module Decidim
       # This controller is the abstract class from which all other controllers of
       # this engine inherit.
       class ApplicationController < Decidim::Admin::ApplicationController
+        register_permissions(::Decidim::ActionDelegator::Admin::ApplicationController,
+                             ::Decidim::ActionDelegator::Permissions,
+                             ::Decidim::Admin::Permissions)
+        
         def permission_class_chain
-          [::Decidim::ActionDelegator::Permissions] + super
+          ::Decidim.permissions_registry.chain_for(::Decidim::ActionDelegator::Admin::ApplicationController)
         end
       end
     end
