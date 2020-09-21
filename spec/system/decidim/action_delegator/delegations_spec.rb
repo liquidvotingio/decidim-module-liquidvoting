3fc17a32a0a6d508bc9e05aa43cd8a0a7a48c86b
Pau Perez <saulopefa@gmail.com>
2020-09-21 11:45:38 +0200

    Add spec to test delegation unvote

--- spec/system/decidim/action_delegator/delegations_spec.rb
+++ spec/system/decidim/action_delegator/delegations_spec.rb
@@ -11,12 +11,12 @@ describe "Delegation vote", type: :system do
     let(:user) { create(:user, :confirmed, organization: organization) }
 
     context "and authenticated user" do
-      context "and never voted before" do
-        let!(:response) { create :response, question: question }
-        let(:setting) { create(:setting, consultation: consultation) }
-        let(:granter) { create(:user, :confirmed, organization: organization) }
-        let!(:delegation) { create(:delegation, setting: setting, granter: granter, grantee: user) }
+      let!(:response) { create :response, question: question }
+      let(:setting) { create(:setting, consultation: consultation) }
+      let(:granter) { create(:user, :confirmed, organization: organization) }
+      let!(:delegation) { create(:delegation, setting: setting, granter: granter, grantee: user) }
 
+      context "and never voted before" do
         before do
           switch_to_host(organization.host)
           login_as user, scope: :user
@@ -32,7 +32,26 @@ describe "Delegation vote", type: :system do
           click_button translated(response.title)
           click_button "Confirm"
 
-          expect(page).to have_button(class: "delegation_unvote_button", visible: :hidden)
+          click_link(id: "delegations-button")
+          expect(page).to have_button(class: "delegation_unvote_button")
+        end
+      end
+
+      context "and voted before" do
+        let!(:vote) { create(:vote, author: granter, question: question, response: response) }
+
+        before do
+          switch_to_host(organization.host)
+          login_as user, scope: :user
+          visit decidim_consultations.question_path(question)
+        end
+
+        it "lets the user unvote on behalf of some other member" do
+          click_link(id: "delegations-button")
+          click_button(class: "delegation_unvote_button")
+
+          click_link(id: "delegations-button")
+          expect(page).to have_link(class: "delegation-vote-button")
         end
       end
     end
