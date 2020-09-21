916c1df335228efedab368f3bd13b1ddb6121167
Pau Perez <saulopefa@gmail.com>
2020-09-21 12:27:10 +0200

    Test Delegation.granted_to?

--- spec/models/decidim/action_delegator/delegation_spec.rb
+++ spec/models/decidim/action_delegator/delegation_spec.rb
@@ -9,6 +9,27 @@ module Decidim
 
       it { is_expected.to belong_to(:setting) }
       it { is_expected.to be_valid }
+
+      describe ".granted_to?" do
+        let(:user) { create(:user) }
+        let(:organization) { create(:organization) }
+        let(:consultation) { create(:consultation, :active, organization: organization) }
+        let(:setting) { create(:setting, consultation: consultation) }
+
+        context "when there is no delegation granted to user for the given consultation" do
+          it "returns false" do
+            expect(described_class.granted_to?(user, consultation)).to eq(false)
+          end
+        end
+
+        context "when there are delegations granted to user for the given consultation" do
+          let!(:delegation) { create(:delegation, setting: setting, grantee: user) }
+
+          it "returns true" do
+            expect(described_class.granted_to?(user, consultation)).to eq(true)
+          end
+        end
+      end
     end
   end
 end
