2ac92813c32dd1781e58ed4c08aa1d2d63f3c344
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-11 20:10:26 +0100

    Remove verifications workflow file

--- lib/decidim/liquidvoting/workflow.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "decidim/verifications"
-
-Decidim::Verifications.register_workflow(:delegations_verifier) do |workflow|
-  workflow.engine = Decidim::Liquidvoting::Verification::Engine
-  workflow.admin_engine = Decidim::Liquidvoting::Verification::AdminEngine
-end
