e11041569847529e0e105450844b332c3c7d1850
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-11 20:24:45 +0100

    Add liquidvoting.io client

--- lib/decidim/liquidvoting.rb
+++ lib/decidim/liquidvoting.rb
@@ -3,6 +3,7 @@
 require "decidim/liquidvoting/admin"
 require "decidim/liquidvoting/admin_engine"
 require "decidim/liquidvoting/engine"
+require "decidim/liquidvoting/client"
 
 module Decidim
   # This namespace holds the logic of the `Liquidvoting` module
