1a363f5e3df65af4a6a18e04e504121524dac6e1
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-11 19:52:01 +0100

    Change Decidim version to 0.22

--- lib/decidim/liquidvoting/version.rb
+++ lib/decidim/liquidvoting/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-liquidvoting version.
   module Liquidvoting
-    DECIDIM_VERSION = "0.21.0"
+    DECIDIM_VERSION = "0.22.0"
     VERSION = "0.1"
   end
 end
