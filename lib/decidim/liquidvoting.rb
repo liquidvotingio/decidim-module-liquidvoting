593454333344400ddc21ea9617d99eabf63262da
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-23 18:00:44 +0100

    Register global engine inside component.rb
    and make naming of component and engine consistent with each other. Still don't see the component in the admin pull down, though :P

--- lib/decidim/liquidvoting.rb
+++ lib/decidim/liquidvoting.rb
@@ -10,20 +10,3 @@ module Decidim
   module Liquidvoting
   end
 end
-
-
-# User space engine, used mostly in the context of proposal voting to let users
-# manage their delegations
-Decidim.register_global_engine(
-  :decidim_liquidvoting, # this is the name of the global method to access engine routes
-  ::Decidim::Liquidvoting::Engine,
-  at: "/liquidvoting"
-)
-
-# # Admin side of the delegations management. Admins can overlook all delegations and
-# # create their own
-# Decidim.register_global_engine(
-#   :decidim_admin_liquidvoting,
-#   ::Decidim::Liquidvoting::AdminEngine,
-#   at: "/admin/liquidvoting"
-# )
