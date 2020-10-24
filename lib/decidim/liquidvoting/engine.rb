315a7a378eeae2f98cfb753df212395f21cf4ca7
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-24 01:30:52 +0100

    Remove unneeded initializer

--- lib/decidim/liquidvoting/engine.rb
+++ lib/decidim/liquidvoting/engine.rb
@@ -32,15 +32,6 @@ module Decidim
       initializer "decidim_liquidvoting.assets" do |app|
         app.config.assets.precompile += %w(decidim_liquidvoting_manifest.js decidim_liquidvoting_manifest.css)
       end
-
-      # initializer "decidim.user_menu" do
-      #   Decidim.menu :user_menu do |menu|
-      #     menu.item t("vote_delegations", scope: "layouts.decidim.user_profile"),
-      #               decidim_liquidvoting.user_delegations_path,
-      #               position: 5.0,
-      #               active: :exact
-      #   end
-      # end
     end
   end
 end
