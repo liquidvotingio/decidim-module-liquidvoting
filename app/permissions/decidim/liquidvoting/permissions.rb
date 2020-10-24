ae59d5be7c9c44e6b43013d69fbee60c70c6bc51
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2020-10-24 01:04:36 +0100

    Further cleanup

--- app/permissions/decidim/liquidvoting/permissions.rb
+++ app/permissions/decidim/liquidvoting/permissions.rb
@@ -6,7 +6,6 @@ module Decidim
       def permissions
         return permission_action unless user.admin?
         return permission_action unless permission_action.scope == :admin
-        return permission_action unless [:delegation, :setting].include?(permission_action.subject)
 
         allow! if can_perform_action?(permission_action.action, resource)
 
