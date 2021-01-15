d8d1bcf2d24bd773ea73aac1206aac0c162caf8f
Dave Frey <dfrey@hey.com>
2021-01-15 15:39:31 +0100

    Make module naming consistent
    Long names for repo urls, all rest are short (no -module-) names

--- lib/decidim/liquidvoting/version.rb
+++ lib/decidim/liquidvoting/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This holds the decidim-module-liquidvoting version.
+  # This holds the decidim-liquidvoting version.
   module Liquidvoting
     DECIDIM_VERSION = "0.22.0"
     VERSION = "0.1"
