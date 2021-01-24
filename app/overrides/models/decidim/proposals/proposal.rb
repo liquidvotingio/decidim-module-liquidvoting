dc79b6df3f96ba4cb0b011eb4d6eef49054a0a33
Dave Frey <dfrey@hey.com>
2021-01-25 00:38:04 +0100

    Cleanup the deprecated Proposal model override
    Both remaining methods now log a message, then continue.
    Still unsure if we back out this override completely (and so manage the proposals.proposal_votes_count)
    or abandon completely proposals.proposal_votes_count by removing all references to these two methods

--- app/overrides/models/decidim/proposals/proposal.rb
+++ app/overrides/models/decidim/proposals/proposal.rb
@@ -7,18 +7,21 @@ Decidim::Proposals::Proposal.class_eval do
   #
   # rubocop:disable Rails/SkipsModelValidations
   def update_votes_count(component)
-# TODO: remove this :proposal_votes_count updater"
-msg = "Proposal#update_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
-Rails.logger.warn msg
-# fail msg
+    # TODO: remove this :proposal_votes_count updater"
+    msg = "BOOM Proposal#update_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
+    Rails.logger.warn msg
+    # fail msg
+
     update_columns(proposal_votes_count: get_progress(component))
   end
 
   def proposal_votes_count
-# TODO: remove this :proposal_votes_count accessor"
-msg = "Proposal#proposal_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
-Rails.logger.warn msg
-# fail msg
+    # TODO: remove this :proposal_votes_count accessor"
+    msg = "BOOM Proposal#proposal_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
+    Rails.logger.warn msg
+    # fail msg
+
+    super
   end
 
   # rubocop:enable Rails/SkipsModelValidations
