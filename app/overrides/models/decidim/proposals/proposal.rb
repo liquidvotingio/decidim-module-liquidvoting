ced56c4fc7c0ade3e4efc5dcdd128ed68cc2f03d
Dave Frey <dfrey@hey.com>
2021-01-24 22:27:22 +0100

    Move Proposal deprecations to logfile warnings

--- app/overrides/models/decidim/proposals/proposal.rb
+++ app/overrides/models/decidim/proposals/proposal.rb
@@ -16,13 +16,17 @@ Decidim::Proposals::Proposal.class_eval do
   # rubocop:disable Rails/SkipsModelValidations
   def update_votes_count(component)
 # TODO: remove this :proposal_votes_count updater"
-fail "This module uses Liquidvoting to determine proposal votes count; this code path is probably obsolete"
+msg = "Proposal#update_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
+Rails.logger.warn msg
+# fail msg
     update_columns(proposal_votes_count: get_progress(component))
   end
 
   def proposal_votes_count
 # TODO: remove this :proposal_votes_count accessor"
-fail "This module uses Liquidvoting to determine proposal votes count; this code path is probably obsolete"
+msg = "Proposal#proposal_votes_count: This module uses Liquidvoting to manage proposal votes count; this code path is probably obsolete"
+Rails.logger.warn msg
+# fail msg
   end
 
   # rubocop:enable Rails/SkipsModelValidations
