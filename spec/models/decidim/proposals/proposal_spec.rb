46b28476288d3eec89f889ee0a2e0c2408e226e2
Dave Frey <dfrey@hey.com>
2021-03-11 22:42:27 +0100

    Cleanup proposal_spec

--- spec/models/decidim/proposals/proposal_spec.rb
+++ spec/models/decidim/proposals/proposal_spec.rb
@@ -2,32 +2,26 @@
 
 require "spec_helper"
 
-module Decidim
-  module Proposals
-    describe Proposal do
-      subject { proposal }
+describe Decidim::Proposals::Proposal do
+  subject { create(:proposal) }
 
-      let(:proposal) { create(:proposal) }
+  it { is_expected.to be_valid }
 
-      it { is_expected.to be_valid }
+  describe "#update_votes_count" do
+    let(:current_vote_count) { 35 }
 
-      describe "#update_votes_count" do
-        let(:current_vote_count) { 35 }
+    context "when we update the vote count" do
+      before do
+        subject.update_votes_count(current_vote_count)
+        subject.reload
+      end
 
-        context "when we update the vote count" do
-          before do
-            subject.update_votes_count(current_vote_count)
-            subject.reload
-          end
+      it "is updated" do
+        expect(subject.proposal_votes_count).to eq(current_vote_count)
+      end
 
-          it "is updated" do
-            expect(proposal.proposal_votes_count).to eq(current_vote_count)
-          end
-
-          it "is independent of the :votes association" do
-            expect(proposal.votes.count).to eq(0)
-          end
-        end
+      it "is independent of the :votes association" do
+        expect(subject.votes.count).to eq(0)
       end
     end
   end
