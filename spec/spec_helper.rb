4c0340b9f41920d6959f7b4d1ef5e516bdb6cc94
Dave Frey <dfrey@hey.com>
2021-03-12 17:20:19 +0100

    Workaround WebMock "too many open files"
    Solution proposed by Capybara https://github.com/teamcapybara/capybara#gotchas is to allow_net_connect.
    
    It works, but overrides what Decidim explicitly does in decidim-dev/lib/decidim/dev/test/rspec_support/webmock.rb
      WebMock.disable_net_connect!(allow_localhost: true)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,6 @@ Decidim::Dev.dummy_app_path =
   File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
+
+# Override decidim-dev's webmock.rb to workaround "Too many open files" problem
+WebMock.allow_net_connect!(net_http_connect_on_start: true)
