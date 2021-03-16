25f77dfcf47ac8b50bfbe9ce913b4ea776580bd8
Oliver Azevedo Barnes <oliverbarnes@hey.com>
2021-03-16 00:41:47 +0000

    Fix loading of the api schema in spec

--- spec/system/support_proposal_spec.rb
+++ spec/system/support_proposal_spec.rb
@@ -3,31 +3,28 @@
 require "spec_helper"
 
 describe "Supporting a Proposal", :vcr, type: :system do
-  include_context "with a component"
-  let!(:component) do
-    create(
-      :proposal_component,
-      :with_votes_enabled,
-      participatory_space: participatory_space
-    )
-  end
-  let!(:proposal) { create :proposal, component: component }
-  let!(:user) { create(:user, :confirmed, organization: organization) }
+  VCR.use_cassette('fetch_schema') do 
+    include_context "with a component"
+    let!(:component) do
+      create(
+        :proposal_component,
+        :with_votes_enabled,
+        participatory_space: participatory_space
+      )
+    end
+    let!(:proposal) { create :proposal, component: component }
+    let!(:user) { create(:user, :confirmed, organization: organization) }
 
-  def visit_proposal
-    visit resource_locator(proposal).path
-  end
+    def visit_proposal
+      visit resource_locator(proposal).path
+    end
 
-  before do
-    login_as user, scope: :user
-    visit_proposal
-  end
+    before do
+      login_as user, scope: :user
+      visit_proposal
+    end
 
-  it "works" do
-    VCR.use_cassette('create_vote') do
-      # If the api is running locally, this passes.
-      # We need to add VCR
-byebug
+    it "works" do
       click_button("Support", id: "vote_button-#{proposal.id}")
       expect(page).to have_button("Already supported")
     end
