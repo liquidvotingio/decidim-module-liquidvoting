6eef7216a64ec9dd7908f41667d6f89ed9b7ddc0
Simon Tharby <jinjamail@gmail.com>
2021-03-24 14:48:39 +0000

    Include delegation ui elements in unsupport_proposal_spec.

--- spec/system/unsupport_proposal_spec.rb
+++ spec/system/unsupport_proposal_spec.rb
@@ -27,5 +27,8 @@ describe "Unsupporting a Proposal", type: :system do
   it "works" do
     click_button("Already supported", id: "vote_button-#{proposal.id}")
     expect(page).to have_button("Support", id: "vote_button-#{proposal.id}")
+    expect(page).to have_button("Delegate Support")
+    expect(page).to have_select("delegate_email")
+    expect(page).to have_text(:visible, /Or delegate your support:/)
   end
 end
