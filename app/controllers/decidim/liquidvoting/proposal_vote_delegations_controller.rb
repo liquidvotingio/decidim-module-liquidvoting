908b185c01516737f75006fdf0efa09426d21435
Dave Frey <dfrey@hey.com>
2021-04-09 17:08:28 +0200

    Illustrate a different pattern for lv_state

--- app/controllers/decidim/liquidvoting/proposal_vote_delegations_controller.rb
+++ app/controllers/decidim/liquidvoting/proposal_vote_delegations_controller.rb
@@ -5,17 +5,17 @@ module Decidim
     class ProposalVoteDelegationsController < Decidim::Proposals::ApplicationController
       before_action :authenticate_user!
 
-      helper_method :proposal, :proposal_path, :proposal_proposal_vote_path
+      helper_method :proposal, :proposal_path, :proposal_proposal_vote_path, :lv_state
 
       def create
         enforce_permission_to :vote, :proposal, proposal: proposal
 
         Liquidvoting.create_delegation(delegator_email, params[:delegate_email], proposal)
+        refresh_lv_state
 
         @from_proposals_list = params[:from_proposals_list] == "true"
         @proposals = [] + [proposal]
 
-        @lv_state = Liquidvoting.user_proposal_state(delegator_email, proposal_locator.url)
         render "decidim/proposals/proposal_votes/update_buttons_and_counters"
       end
 
@@ -29,11 +29,11 @@ module Decidim
         enforce_permission_to :unvote, :proposal, proposal: proposal
 
         Liquidvoting.delete_delegation(delegator_email, params[:delegate_email], proposal)
+        refresh_lv_state
 
         @from_proposals_list = params[:from_proposals_list] == "true"
         @proposals = [] + [proposal]
 
-        @lv_state = Liquidvoting.user_proposal_state(delegator_email, proposal_locator.url)
         render "decidim/proposals/proposal_votes/update_buttons_and_counters"
       end
 
@@ -59,6 +59,15 @@ module Decidim
 
       def proposal_proposal_vote_path(_ignore)
         "#{proposal_path(nil)}/proposal_vote"
+      end
+
+      attr_reader :lv_state
+
+      # Retrieve the current liquidvoting state. The state is exposed as a helper method :lv_state.
+      # Since timing with regard to votes and delegations is important, make this a deliberate act,
+      # rather than a lazy memoized attribute.
+      def refresh_lv_state
+        @lv_state = Liquidvoting.user_proposal_state(delegator_email, proposal_locator.url)
       end
     end
   end
