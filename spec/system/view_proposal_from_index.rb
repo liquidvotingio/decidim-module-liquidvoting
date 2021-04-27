1bca9ec242afa3bad31c7cc6c98dd4dc5f72e488
Simon Tharby <jinjamail@gmail.com>
2021-04-27 15:38:52 +0100

    Improve spec by splitting 1 action into 2 constituent actions.
    This, plus more explicit comments, clarifies that we are asserting
    differences from core Decidim behavior.

--- spec/system/view_proposal_from_index.rb
+++ spec/system/view_proposal_from_index.rb
@@ -2,9 +2,10 @@
 
 require "spec_helper"
 
-# Test to confirm that a proposal card in the proposals index view does not include the 'Support'
-# button or our 'Delegate Support' button, even when these are visible in the show proposal view.
-describe "View a Proposal by clicking 'view proposal' on index view proposal card", type: :system do
+# Test to confirm that override presenting the Support button on a proposal card in the proposals
+# index view, because we don't yet have a good liquidvoting UI for doing that from the Proposals
+# list page. Instead, we want to replace the voting related elements with a View Proposal button.
+describe "Viewing the Proposals list", type: :system do
   include_context "with a component"
   let!(:component) do
     create(
@@ -21,11 +22,14 @@ describe "View a Proposal by clicking 'view proposal' on index view proposal car
     visit_component
   end
 
-  it "works" do
+  it "replaces the voting footer on the proposal card with a View Proposal button" do
     expect(page).to have_content("PROPOSALS")
     expect(page).to have_content("VIEW PROPOSAL")
     expect(page).not_to have_button("Support")
     expect(page).not_to have_button("Delegate Support")
+  end
+
+  it "goes to the Proposal page when clicking View Proposal button" do
     click_link("View proposal")
     expect(page).to have_button("Support", id: "vote_button-#{proposal.id}")
     expect(page).to have_button("Delegate Support")
