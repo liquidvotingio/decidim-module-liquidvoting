633f0056ce5d64ad7d7f5aa101a3a2d9f4d4b6eb
Dave Frey <dave@liquidvoting.io>
2021-05-20 12:37:49 +0200

    Assert an assembly proposal can be supported
    This is pre-work to asserting LV behavior on Assembly proposals,
    in addition to Participatory Process proposals.
    
    It's red because it doesn't find the Support button, this is most likely a dom id problem.
    
    It's also depending on side effects from setting up a Process proposal, that shouldn't be needed

---
+++ spec/system/support_assembly_proposal_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Supporting an Assembly Proposal", type: :system do
+  include_context "with a component"
+  let!(:component) do
+    create(
+      :proposal_component,
+      :with_votes_enabled,
+      participatory_space: participatory_space
+    )
+  end
+  let!(:proposal) { create :proposal, component: component }
+
+  let!(:user) { create(:user, :confirmed, organization: organization) }
+
+  let!(:assembly) do
+    create(
+      :assembly,
+      :with_type,
+      organization: organization,
+      description: { en: "Description", ca: "Descripció", es: "Descripción" },
+      short_description: { en: "Short description", ca: "Descripció curta", es: "Descripción corta" },
+      show_statistics: true
+    )
+  end
+  let(:manifest_name) { :assemblies }
+  let!(:proposals_component) { create(:component, :published, participatory_space: assembly, manifest_name: :proposals) }
+  let!(:assembly_proposal) { create :proposal, component: component }
+
+  def visit_assembly_proposal
+    visit resource_locator(assembly_proposal).path
+  end
+
+  before do
+    login_as user, scope: :user
+    visit_assembly_proposal
+  end
+
+  it "works" do
+    click_button("Support", id: "vote_button-#{proposal.id}")
+
+    expect(page).to have_button("Already supported")
+    expect(page).not_to have_select("delegate_id")
+    expect(page).to have_button("Delegate Support", disabled: true)
+  end
+end
