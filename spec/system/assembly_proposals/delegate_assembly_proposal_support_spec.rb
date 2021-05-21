8d39efba5115dabac86abebd4365aaf6e26b4545
Dave Frey <dave@liquidvoting.io>
2021-05-21 11:50:19 +0200

    Repair fixtures for all assembly proposal specs

--- spec/system/assembly_proposals/delegate_assembly_proposal_support_spec.rb
+++ spec/system/assembly_proposals/delegate_assembly_proposal_support_spec.rb
@@ -3,19 +3,13 @@
 require "spec_helper"
 
 describe "Delegating support for a Proposal", type: :system do
-  include_context "with a component"
-  let!(:component) do
-    create(
-      :proposal_component,
-      :with_votes_enabled,
-      participatory_space: participatory_space
-    )
-  end
+  let(:organization) { create(:organization) }
+  let(:assembly) { create(:assembly, organization: organization) }
+  let(:assembly_proposals_component) { create(:component, participatory_space: assembly, manifest_name: :proposals) }
+  let(:assembly_proposal) { create :proposal, component: assembly_proposals_component }
 
-  let!(:user) { create(:user, :confirmed, organization: organization) }
-  let(:manifest_name) { :assemblies }
-  let!(:assembly_proposal) { create :proposal, component: component }
-  let!(:delegate) { create(:user, :confirmed, organization: organization) }
+  let(:user) { create(:user, :confirmed, organization: organization) }
+  let(:delegate) { create(:user, :confirmed, organization: organization) }
 
   def visit_proposal
     visit resource_locator(assembly_proposal).path
--- spec/system/assembly_proposals/support_assembly_proposal_spec.rb
+++ spec/system/assembly_proposals/support_assembly_proposal_spec.rb
@@ -3,25 +3,19 @@
 require "spec_helper"
 
 describe "Supporting an Assembly Proposal", type: :system do
-  include_context "with a component"
-  let!(:component) do
-    create(
-      :proposal_component,
-      :with_votes_enabled,
-      participatory_space: participatory_space
-    )
-  end
+  let(:organization) { create(:organization) }
+  let(:assembly) { create(:assembly, organization: organization) }
+  let(:assembly_proposals_component) { create(:component, participatory_space: assembly, manifest_name: :proposals) }
+  let(:assembly_proposal) { create :proposal, component: assembly_proposals_component }
 
-  let!(:user) { create(:user, :confirmed, organization: organization) }
-  let(:manifest_name) { :assemblies }
-  let!(:assembly_proposal) { create :proposal, component: component }
+  let(:user) { create(:user, :confirmed, organization: organization) }
 
   def visit_assembly_proposal
     visit resource_locator(assembly_proposal).path
   end
 
   before do
-    expect(component.participatory_space_type).to eq("Decidim::Assembly")
+    expect(assembly_proposal.component.participatory_space_type).to eq("Decidim::Assembly")
 
     login_as user, scope: :user
     visit_assembly_proposal
